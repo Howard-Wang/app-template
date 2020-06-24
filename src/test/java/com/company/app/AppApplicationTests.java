e756b50ceb5c39ead9675fddd4c2a5e672492aab
wanghao <wanghao5@vrvmail.com.cn>
2020-06-24 16:52:59 +0800

    屏蔽无用的单元测试

--- src/test/java/com/company/app/AppApplicationTests.java
+++ src/test/java/com/company/app/AppApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class AppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
